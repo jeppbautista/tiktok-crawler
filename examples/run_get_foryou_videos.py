f5cbf69e8363ffcf99ab24348334b5ab13c0ecfe
Jepp Bautista <slapadasbas@gmail.com>
2022-12-31 01:00:00 +0800

    feat: added sample_usage

--- examples/run_get_foryou_videos.py
+++ examples/run_get_foryou_videos.py
@@ -1,9 +1,6 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
 from tiktok_crawler.crawler import Crawler
 
 crawl = Crawler(limit=5)
 tiktoks = crawl.get_foryou_tiktok_videos()
 for tiktok in tiktoks:
-    tiktok.save(path="./output")
+    tiktok.save(path="../output")
